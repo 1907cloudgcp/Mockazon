3579e312dd22ec925fa21d961e316253d8329f93
Blake Anthony Mongeon <bamongeon@gmail.com>
2019-09-10 15:12:41 -0400

    changed method of downloading gcloud

--- install.sh
+++ install.sh
@@ -2,5 +2,13 @@
 #!/bin/sh
 
 # Install kubernetes and set config
-yes | gcloud components update
-yes | gcloud components install kubectl
+
+if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
+  rm -rf "$HOME/google-cloud-sdk"
+  curl https://sdk.cloud.google.com | bash > /dev/null
+fi
+
+source $HOME/google-cloud-sdk/path.bash.inc
+
+# yes | gcloud components update
+yes | gcloud components update kubectl
