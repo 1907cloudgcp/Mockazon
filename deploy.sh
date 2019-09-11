0cf3ff756ab7dfcf1f80eaf0516ba5949ddde2a9
Blake Anthony Mongeon <bamongeon@gmail.com>
2019-09-11 11:54:57 -0400

    added dot to docker build command

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ yes | gcloud components install kubectl
 # gcloud container clusters get-credentials mockazon --zone us-central1-a --project gcpdemosbmongeon
 gcloud auth configure-docker
 
-docker build -t gcr.io/gcpdemosbmongeon/mockazon:v1.0
+docker build -t gcr.io/gcpdemosbmongeon/mockazon:v1.0 .
 docker push gcr.io/gcpdemosbmongeon/mockazon:v1.0
 echo "k8s.yml"
 kubectl apply -f k8s.yml
