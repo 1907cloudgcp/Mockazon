114e3998564804aa80fbd8be8548df943eb4babe
Blake Anthony Mongeon <bamongeon@gmail.com>
2019-09-11 15:56:35 -0400

    made ls recursive

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ yes | gcloud components install kubectl
 # gcloud container clusters get-credentials mockazon --zone us-central1-a --project gcpdemosbmongeon
 gcloud auth configure-docker
 
-ls
+ls -r
 
 docker build -t gcr.io/gcpdemosbmongeon/mockazon:v1.0 .
 docker push gcr.io/gcpdemosbmongeon/mockazon:v1.0
