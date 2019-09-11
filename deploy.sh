53998f12e96dd6b17f9128b47fa3ba72879cc95e
Blake Anthony Mongeon <bamongeon@gmail.com>
2019-09-11 10:46:24 -0400

    moved all commands to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,24 @@
-#!/usr/bin/env bash
+# deploy.sh
+#!/bin/sh
+
+# Install kubernetes and set config
+
+if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
+  rm -rf "$HOME/google-cloud-sdk"
+  curl https://sdk.cloud.google.com | bash > /dev/null
+fi
+
+source $HOME/google-cloud-sdk/path.bash.inc
+
+# yes | gcloud components update
+yes | gcloud components install kubectl
+
+gcloud auth activate-service-account --key-file=gcpdemosbmongeon-e25e1414c004.json
+
+gcloud config set project gcpdemosbmongeon
+gcloud container clusters get-credentials mockazon --zone us-central1-a --project gcpdemosbmongeon
+# gcloud auth configure-docker
 
 #docker build -t gcr.io/gcpdemosbmongeon/mockazon:v1.0
 #docker push gcr.io/gcpdemosbmongeon/mockazon:v1.0
-yes | gcloud components install kubectl
 kubectl apply -f k8s.yml
