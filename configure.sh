7af8edcd1e48ef3cb1a0407636bfe2255cb910f3
Blake Anthony Mongeon <bamongeon@gmail.com>
2019-09-12 14:08:18 -0400

    made argument zonal again

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 gcloud config set project gcpdemosbmongeon
-gcloud container clusters get-credentials mockazon --zone us-central1 --project gcpdemosbmongeon
+gcloud container clusters get-credentials mockazon --zone us-central1-a --project gcpdemosbmongeon
 # gcloud auth configure-docker
