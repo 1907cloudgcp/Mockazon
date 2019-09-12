b67572f7fabf082e68662b0441028418a1330a5b
Blake Anthony Mongeon <bamongeon@gmail.com>
2019-09-12 14:02:02 -0400

    removed regional argument

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 gcloud config set project gcpdemosbmongeon
-gcloud container clusters get-credentials mockazon --region=us-central1 --project gcpdemosbmongeon
+gcloud container clusters get-credentials mockazon --project gcpdemosbmongeon
 # gcloud auth configure-docker
